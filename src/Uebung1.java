029b95b19be7d613f0bc01d73e7db235ec7e2520
StefanBerger27 <01530856@edu.campus02.at>
2023-09-29 15:38:34 +0200

    Commit

---
+++ src/Uebung1.java
@@ -0,0 +1,8 @@
+public class Uebung1 {
+    public static void main(String[] args) {
+        // code completion: strg + Leertaste
+        System.out.println("Stefan Berger");
+        System.out.println("Schlipfkrapfen");
+        System.out.println("Freitag");
+    }
+}
